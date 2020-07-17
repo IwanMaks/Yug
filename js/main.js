2044e4407e94aeb34cc971abedfec9a5a9da19c7
prostochlen <iwan.maksackov@yandex.ru>
2020-07-17 22:27:23 +0300

    1.02.05

--- js/main.js
+++ js/main.js
@@ -8,6 +8,11 @@ const logoName = document.querySelector('.logo-name');
 const logoDescription = document.querySelector('.logo-description');
 const socialLink = document.querySelector('.social-link');
 
+const promoButton = document.querySelector('.promo-button');
+const buttonContainer = document.querySelector('.button-container');
+const cardForage = document.querySelector('.card-forage');
+const modal = document.querySelector('.modal');
+
 window.addEventListener('scroll', () => { 
     console.log(pageYOffset);
     if(pageYOffset >= 50 && screen.width > 768){
@@ -20,6 +25,8 @@ window.addEventListener('scroll', () => {
         logoDescription.style.fontSize = '28px';
         logoDescription.style.fontWeight = '300';
         logoDescription.style.lineHeight = '33px';
+        logoDescription.style.marginLeft = '0';
+        logoDescription.style.marginTop = '0';
     } else {
         header.classList.remove('white');
         logo.classList.remove('logo-mini');
@@ -30,5 +37,8 @@ window.addEventListener('scroll', () => {
         logoDescription.style.fontSize = '14px';
         logoDescription.style.fontWeight = 'normal';
         logoDescription.style.lineHeight = '16px';
+        logoDescription.style.marginLeft = '2px';
+        logoDescription.style.marginTop = '-10px'
     }
 });
+
